541ebef2c8fffdb6d5319944821043673e4b3923
Chris V <cvaldezchavez@gmail.com>
2025-04-24 01:49:12 -0500

    update Fido's back

--- dogdoor/DogDoorSimulator.java
+++ dogdoor/DogDoorSimulator.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 public class DogDoorSimulator {
     
     public static void main(String[] args) {
@@ -10,6 +12,16 @@ public class DogDoorSimulator {
         System.out.println("Fido has gone outside...");
 
         System.out.println("Fido's all done...");
+
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("... but he's stuck outside!");
+        System.out.println("Fido's starts barking ...");
+        System.out.println("... Gina grabs the remote control.");
+        remote.pressButton();
 
         System.out.println("Fido's back inside...");
     }
